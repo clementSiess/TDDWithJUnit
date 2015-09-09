45de986c93af31698fb16b8fd6ca19bf3f79cc1a
clementSiess <elmusicos@hotmail.fr>
2015-09-09 14:34:30 -0500

    implemented decelerate method

--- src/Car.java
+++ src/Car.java
@@ -37,6 +37,15 @@ public class Car {
 		}
 	}
 	
+	public void decelerate(int amount){
+		int limit = this.maxSpeed - this.speed;
+
+		if (this.speed <= limit){
+			this.speed -= amount;
+		}
+	}
+	
+	
 	/**
 	 * Displays the object as a string in the format
 	 * "Color: red, Speed: 100"
